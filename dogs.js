c71b3eec7608c24ce07274957dcba1fab6043274
Aaron Wilder <element1028@gmail.com>
2016-06-15 14:56:38 -0700

    Update dogs.js

--- dogs.js
+++ dogs.js
@@ -1,31 +1,12 @@
-var dogName = "rover";
-var dogWeight = 23;
-if (dogWeight > 20) {
-    console.log(dogName + " says WOOF WOOF");
-} else {
-    console.log(dogName + " says woof woof");
+function bark( name, weight ) {
+    if ( weight > 20) {
+        console.log( name + " says WOOF WOOF" );
+    } else {
+        console.log(name + " says woof woof");
+    }
 }
 
-var dogName = "spot";
-var dogWeight = 13;
-if (dogWeight > 20) {
-    console.log(dogName + " says WOOF WOOF");
-} else {
-    console.log(dogName + " says woof woof");
-}
-
-var dogName = "spike";
-var dogWeight = 53;
-if (dogWeight > 20) {
-    console.log(dogName + " says WOOF WOOF");
-} else {
-    console.log(dogName + " says woof woof");
-}
-
-var dogName = "lady";
-var dogWeight = 17;
-if (dogWeight > 20) {
-    console.log(dogName + " says WOOF WOOF");
-} else {
-    console.log(dogName + " says woof woof");
-}
+bark("rover", 23);
+bark("spot", 13);
+bark("spike", 53);
+bark("lady", 17);
