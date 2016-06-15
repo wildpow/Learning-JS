05179bfab3f001baf45fffaeaf95aeebaff2be90
Aaron Wilder <element1028@gmail.com>
2016-06-15 14:52:02 -0700

    Add files via upload

---
+++ dogs.js
@@ -0,0 +1,31 @@
+var dogName = "rover";
+var dogWeight = 23;
+if (dogWeight > 20) {
+    console.log(dogName + " says WOOF WOOF");
+} else {
+    console.log(dogName + " says woof woof");
+}
+
+var dogName = "spot";
+var dogWeight = 13;
+if (dogWeight > 20) {
+    console.log(dogName + " says WOOF WOOF");
+} else {
+    console.log(dogName + " says woof woof");
+}
+
+var dogName = "spike";
+var dogWeight = 53;
+if (dogWeight > 20) {
+    console.log(dogName + " says WOOF WOOF");
+} else {
+    console.log(dogName + " says woof woof");
+}
+
+var dogName = "lady";
+var dogWeight = 17;
+if (dogWeight > 20) {
+    console.log(dogName + " says WOOF WOOF");
+} else {
+    console.log(dogName + " says woof woof");
+}
